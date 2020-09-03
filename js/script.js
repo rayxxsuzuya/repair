5c17ee9421f78e300ed10ad62e8dd5bdd60dffc9
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2020-09-03 18:53:09 +0300

    contacts & price adaptive

--- js/script.js
+++ js/script.js
@@ -3,4 +3,18 @@ $('.works__slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
+    responsive: [
+      {
+        breakpoint: 740,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 470,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
